28fa51da4ff367241e6239bb812b0bddc60771bb
Mohammed Alsakabi <mohammed.alsakabi@gmail.com>
2024-08-29 02:59:25 +0300

    Update PINNs.m

--- PINNs.m
+++ PINNs.m
@@ -1,16 +1,11 @@
 clc; clear all; close all
-%% Parameters:
+%% Basic waves:
+fs = 3000;
 f = 100;
-n = 0:0.02:1;
-wave = exp(-j*2*pi*f*n);
-plot(real(wave));
-
-%%
+t = 0:1/fs:3/f;
 
 %%% circle equation
-omega = 1; %in rps
-fs = 1;
-theta = 0:1/fs:360;
+theta = 0:5:720;
 theta_in_rad = theta*pi/180;
 r = 1;
 
@@ -44,51 +39,89 @@ y_270 = r*sind(theta-270);
 x_288 = r*cosd(theta-288);
 y_288 = r*sind(theta-288);
 
-figure(1)
-grid on
+%% Load Results:
+load('results.mat');
+
+%% Single Blade:
+% figure
+% grid on
+freq_prog = []; % if the receiver is placed along x-axis
+input_waves = [];
+ground_truth_waves = [];
+fourier_response = [];
+result_progress = [];
+ground_truth_progress = [];
+% Create a figure with a specific size
+figure('Position', [0, 100, 1400, 600]); % [left, bottom, width, height]
+pause(1)
 for ii=1:length(theta)
-    % tic
-    z = 1; % blade span
-    subplot(2,2,1)
-    scatter([z*x_0(ii) z*x_180(ii)], [z*y_0(ii) z*y_180(ii)], 'o', 'filled');
-    title('2 Blades', 'FontSize', 20)
+    subplot(2,3,1)
+    cla; % Clears the current axes
+    z = -1:0.01:1; % blade span
+    scatter([z(1:end-1)*x_0(ii)], [z(1:end-1)*y_0(ii)], 'o', 'filled'); hold on
+    scatter(z(end)*x_0(ii), z(end)*y_0(ii), 'o', 'filled')
+    title('2 Blades Rotor - on x,y plane', 'FontSize', 14)
+    xlabel('x', 'FontSize', 14)
+    ylabel('y', 'FontSize', 14)
+    legend('Blade Discrete Points','Tip point for Velocity Estimation')
     xlim([-2 2])
     ylim([-2 2])
+    annotation('arrow', [0.179, 0.129], [0.75, 0.75], 'LineWidth', 2);
+
+    % Add text above the arrow
+    % The position is in normalized figure coordinates (0 to 1)
+    text(-1.5, 0.1, 'Receiver', 'HorizontalAlignment', 'center', 'Color', 'k', 'FontSize', 12);
     grid on
 
-    subplot(2,2,2)
-    scatter([z*x_0(ii) z*x_120(ii) z*x_240(ii)], [z*y_0(ii) z*y_120(ii) z*y_240(ii)], 'o', 'filled');
-    title('3 Blades', 'FontSize', 20)
+    subplot(2,3,2)
+    scatter([x_0(ii) 0], [0, y_0(ii)], 'o', 'filled')
     xlim([-2 2])
     ylim([-2 2])
+    title('Tip location on x and y axes', 'FontSize',14)
+    xlabel('x', 'FontSize', 14)
+    ylabel('y', 'FontSize', 14)
     grid on
+
+    freq_prog = [freq_prog y_0(ii)*1000];
     
-    subplot(2,2,3)
-    scatter([x_0(ii)*z x_90(ii)*z x_180(ii)*z x_270(ii)*z], [y_0(ii)*z y_90(ii)*z y_180(ii)*z y_270(ii)*z], 'o', 'filled');
-    title('4 Blades', 'FontSize', 20)
-    xlim([-2 2])
+    received_wave = exp(j*2*pi*freq_prog(end)*t) + 0.3*randn(1,length(t));
+    input_waves = [input_waves transpose(received_wave)];
+    ground_truth_waves = [ground_truth_waves exp(j*2*pi*freq_prog(end)*t')];
+    subplot(2,3,4)
+    plot(real(received_wave))
+    xlim([0 91])
     ylim([-2 2])
+    title('Wave at receiver', 'FontSize',14)
+    xlabel('Time Index', 'FontSize', 14)
+    ylabel('Aplitude', 'FontSize', 14)
     grid on
 
-    subplot(2,2,4)
-    scatter([x_0(ii)*z x_72(ii)*z x_144(ii)*z x_216(ii)*z x_288(ii)*z], [y_0(ii)*z y_72(ii)*z y_144(ii)*z y_216(ii)*z y_288(ii)*z], 'o', 'filled');
-    title('5 Blades', 'FontSize', 20)
-    xlim([-2 2])
-    ylim([-2 2])
-    grid on
-    % toc
-
-    pause(0.01)
-
-    % 
-
-
-    % Calculating linear velocity directly (using y-axis)
     subplot(2,3,5)
-    plot(ones(1,22))
-    
+    fourier = abs(fftshift(fft(received_wave)));
+    % fourier_response = [fourier_response transpose(fourier)];
+    plot(linspace(-1000,1000,91),abs(fourier))
+    xlim([-1000 1000]);
+    ylim([0 100]);
+    title('Instantanous Spectrum of Wave at Receiver', 'FontSize',14)
+    xlabel('Frequency Bin', 'FontSize', 14)
+    ylabel('Power', 'FontSize', 14)
+    grid on
 
-
+    subplot(1,3,3)
+    result_progress = [result_progress result(ii)];
+    ground_truth_progress = [ground_truth_progress grount_truth(ii)];
+    cla; % Clears the current axes
+    plot(ground_truth_progress, 'LineWidth',2); hold on
+    plot(result_progress, 'LineWidth',1)
+    xlim([1 145])
+    ylim([-110 110])
+    title('Instantanous Linear Velocity - Ground Truth vs MLP Prediction', 'FontSize',14)
+    xlabel('Time Index', 'FontSize', 14)
+    ylabel('Velocity (m/s)', 'FontSize', 14)
+    legend('GT', 'MLP')
+    grid on
+    pause(0.01)
 end
+
 
 
